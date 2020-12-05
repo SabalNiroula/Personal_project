b3aa9828d5e26049582e4c514b3adeda00c24399
Sabal Niroula <sabalniroula5555@gmail.com>
2020-12-05 10:48:56 +0545

    converter_route.dart file is also changed to stateful widget

--- lib/converter_route.dart
+++ lib/converter_route.dart
@@ -2,32 +2,33 @@ import 'package:creating_custom_widget/unit.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
-
 /// Converter screen where users can input amounts to convert.
 ///
 /// Currently, it just displays a list of mock units.
 ///
 /// While it is named ConverterRoute, a more apt name would be ConverterScreen,
 /// because it is responsible for the UI at the route's destination.
-class ConverterRoute extends StatelessWidget {
-  /// Units for this [Category].
+
+class ConverterRoute extends StatefulWidget {
   final List<Unit> units;
-   final Color color;
+  final Color color;
 
   /// This [ConverterRoute] requires the color and units to not be null.
-  const ConverterRoute({
-    @required this.units,
-    @required this.color
-  }) : assert(color != null),
-       assert(units != null);
+  const ConverterRoute({@required this.units, @required this.color})
+      : assert(color != null),
+        assert(units != null);
 
   @override
+  _ConverterRouteState createState() => _ConverterRouteState();
+}
+
+class _ConverterRouteState extends State<ConverterRoute> {
+  @override
   Widget build(BuildContext context) {
-    // Here is just a placeholder for a list of mock units
-    final unitWidgets = units.map((Unit unit) {
-      ///The UI of how the color,Text,textSize,etc.. be is made from here 
+    final unitWidgets = widget.units.map((Unit unit) {
+      ///The UI of how the color,Text,textSize,etc.. be is made from here
       return Container(
-        color: color,
+        color: widget.color,
         margin: EdgeInsets.all(8.0),
         padding: EdgeInsets.all(16.0),
         child: Column(
@@ -45,7 +46,8 @@ class ConverterRoute extends StatelessWidget {
       );
     }).toList();
 
-    return ListView(///Finally returning the ListView to populate the screen....
+    return ListView(
+      ///Finally returning the ListView to populate the screen....
       children: unitWidgets,
     );
   }
